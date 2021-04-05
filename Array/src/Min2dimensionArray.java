3b6c98977035e1f0f7c2791501adcfc8ee413c2a
longle2812 <79950541+longle2812@users.noreply.github.com>
2021-04-05 14:24:48 +0700

    Min2dimArray

---
+++ Array/src/Min2dimensionArray.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Min2dimensionArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter row");
+        int row = sc.nextInt();
+        System.out.println("Enter column");
+        int column = sc.nextInt();
+        int arr[][] = new int[row][column];
+        for (int i = 0; i < row; i++){
+            for (int j = 0; j < column; j++){
+                System.out.println("Enter "+i+","+j+" value");
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        int min = arr[0][0];
+        row = 0;
+        column = 0;
+        for (int i = 0; i < arr.length; i++){
+            for (int j =0 ; j <arr[0].length; j++){
+                if (arr[i][j] < min ) {
+                    min = arr[i][j];
+                    row = i;
+                    column = j;
+                }
+            }
+        }
+        System.out.println("Min number is: " +min+ " at row "+row+", column "+column);
+    }
+}
