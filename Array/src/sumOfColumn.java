8f6862967c487e0a4404ed59f404f15b2f60bcc6
longle2812 <79950541+longle2812@users.noreply.github.com>
2021-04-05 14:32:06 +0700

    sumOfColumn

--- Array/src/sumOfColumn.java
+++ Array/src/sumOfColumn.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class sumOfColumn {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] arr = {{1,2,3},{1,2,3},{1,3,4}};
+        int[][] arr = {{1, 2, 3}, {1, 2, 3}, {1, 3, 4}};
         System.out.println("Enter the column:");
         int column = sc.nextInt();
-        int sum =0;
-        for (int i = 0; i < arr.length; i++){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
             sum += arr[i][column];
         }
-        System.out.println("Total is:" +sum);
+        System.out.println("Total is:" + sum);
     }
 }
