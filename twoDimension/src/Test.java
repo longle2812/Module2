feca71bb7c262b5dcaa0fc82a6f7e5e90930a4aa
longle2812 <79950541+longle2812@users.noreply.github.com>
2021-04-08 17:09:41 +0700

    3D

---
+++ twoDimension/src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D();
+        System.out.println(point2D);
+
+        Point2D point2D1 = new Point2D(1,2);
+        System.out.println(point2D1);
+
+        Point3D point3D = new Point3D();
+        System.out.println(point3D);
+
+        Point3D point3D1 = new Point3D(1,2,3);
+        System.out.println(point3D1);
+    }
+}
