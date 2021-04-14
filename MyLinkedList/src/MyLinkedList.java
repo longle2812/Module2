80b783c787b3a36eac20fb0578ee29015bb53db1
longle2812 <79950541+longle2812@users.noreply.github.com>
2021-04-14 15:12:16 +0700

    MyGenericStack

--- MyLinkedList/src/MyLinkedList.java
+++ MyLinkedList/src/MyLinkedList.java
@@ -59,20 +59,51 @@ public class MyLinkedList<E> {
     }
 
     public E remove(int index) {
-        Node current = head;
-        for (int i = 0; i < index; i++) {
-            current = current.next;
+        if (index < 0 || index >= numNodes) return null; // Out of range
+        else if (index == 0) return removeFirst(); // Remove first
+        else if (index == numNodes - 1) return removeLast(); // Remove last
+        else {
+        Node previous = head;
+        for (int i = 0; i < index-1; i++) {
+            previous = previous.next;
         }
-        Node result = current;
-        for (int i = index; i < numNodes-1; i++) {
-            current = current.next;
-        }
-        tail = current;
-        tail.next = null;
+        Node current = previous.next;
+        previous.next = current.next;
         numNodes--;
-        return (E) result;
+        return (E) current.getData();}
     }
 
+    public E removeLast() {
+        if (numNodes == 0) return null; // Nothing to remove
+        else if (numNodes == 1) { // Only one element in the list
+            Node temp = head;
+            head = tail = null; // list becomes empty
+            numNodes = 0;
+            return (E) temp.getData();
+        } else {
+            Node current = head;
+
+            for (int i = 0; i < numNodes - 2; i++)
+                current = current.next;
+
+            Node temp = tail;
+            tail = current;
+            tail.next = null;
+            numNodes--;
+            return (E) temp.getData();
+        }
+    }
+
+    public E removeFirst() {
+        if (numNodes == 0) return null; // Nothing to delete
+        else {
+            Node temp = head; // Keep the first node temporarily
+            head = head.next; // Move head to point to next node
+            numNodes--; // Reduce size by 1
+            if (head == null) tail = null; // List becomes empty
+            return (E) temp.getData(); // Return the deleted element
+        }
+    }
     @Override
     public String toString() {
         String result = "";
@@ -86,5 +117,68 @@ public class MyLinkedList<E> {
             result += thisNode.data + " ";
         }
         return result;
+    }
+
+    public int size(){
+        return numNodes;
+    }
+
+    public E clone() {
+        return (E) head.data;
+    }
+
+    public boolean contains(E o) {
+        boolean check = false;
+        Node current = head;
+        while( current != null) {
+            if (current.getData().equals(o)) {
+                check = true;
+                break;
+            }
+            current = current.next;
+        }
+        return check;
+    }
+
+    public int indexOf(E o){
+        int index = -1;
+        if (contains(o)) {
+            Node current = head;
+            for (int i = 0; i < numNodes; i++) {
+                if (current.getData().equals(o)) {
+                    index = i;
+
+                }
+                current = current.next;
+            }
+        }
+        return index;
+    }
+
+    public E get (int i) {
+        int index = 0;
+        E result = null;
+        Node current = head;
+        while (index <= i) {
+            if (index == i) {
+                result = (E) current.getData();
+            }
+            index++;
+            current =current.next;
+        }
+        return result;
+    }
+
+    public E getFirst() {
+        return (E) head.getData();
+    }
+
+    public E getLast() {
+        return (E) tail.getData();
+    }
+
+    public void clear(){
+        head = tail = null;
+        numNodes = 0;
     }
 }
--- MyLinkedList/src/MyLinkedListTest.java
+++ MyLinkedList/src/MyLinkedListTest.java
@@ -1,12 +1,21 @@
 public class MyLinkedListTest {
     public static void main(String[] args) {
         MyLinkedList<String> myLinkedList = new MyLinkedList<String>("Long");
-        myLinkedList.add(2,"Thanh");
-        myLinkedList.add(3, "Duyet");
-        myLinkedList.add(4,"Ngoc Anh");
+        myLinkedList.add(1, "Thanh");
+        myLinkedList.add(2, "Duyet");
+        myLinkedList.add(3, "Ngoc Anh");
+        myLinkedList.add(4, "Thao");
+
         System.out.println(myLinkedList);
-        myLinkedList.remove(2);
-        System.out.println(myLinkedList);
+//        myLinkedList.remove(1;);
+//        System.out.println(myLinkedList.removeLast());
+//        System.out.println(myLinkedList.removeFirst());
+//        System.out.println(myLinkedList);
+        System.out.println(myLinkedList.contains("Ngoc Anh"));
+        System.out.println(myLinkedList.indexOf("Ngoc Anh"));
+        System.out.println(myLinkedList.get(2));
+        System.out.println(myLinkedList.getFirst());
+        System.out.println(myLinkedList.getLast());
     }
 
 }
