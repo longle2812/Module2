9afd81fe396d372050ce7f76305701b52d6e728c
longle2812 <79950541+longle2812@users.noreply.github.com>
2021-04-19 08:31:36 +0700

    Exercise 16.4

--- Exercise 16.4/src/Main.java
+++ Exercise 16.4/src/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
         PersonManagement personManagement = new PersonManagement();
         personManagement.init();
-        chocies(personManagement);
+        choices(personManagement);
     }
 
-    private static void chocies(PersonManagement personManagement) {
+    private static void choices(PersonManagement personManagement) {
         int choice;
         do {
             displayMenu();
