f51fb0d6cd4cf078c6fcfad0d72a8e633e60fcad
longle2812 <79950541+longle2812@users.noreply.github.com>
2021-04-19 15:17:46 +0700

    BubbleSortByStep

---
+++ BubbleSort2/src/BubbleSortByStep.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class BubbleSortByStep {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter list size:");
+        int size = scanner.nextInt();
+        int[] list = new int[size];
+        System.out.println("Enter " + list.length + " values:");
+        for (int i = 0; i < list.length; i++) {
+            list[i] = scanner.nextInt();
+        }
+        System.out.print("Your input list: ");
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + "\t");
+        }
+        //codes below here
+        System.out.println("\nBegin sort processing...");
+        bubbleSortByStep(list);
+    }
+
+    public static void bubbleSortByStep(int[] list) {
+//        boolean needNextPass = true;
+//        for (int k = 1; k < list.length && needNextPass; k++) {
+//            needNextPass = false;
+//            for (int i = 0; i < list.length - k; i++) {
+//                if (list[i] > list[i + 1]) {
+//                    /* Swap list[i] with list[i + 1] */
+//                    System.out.println("Swap " + list[i] + " with " + list[i + 1]);
+//                    int temp = list[i];
+//                    list[i] = list[i + 1];
+//                    list[i + 1] = temp;
+//
+//                    needNextPass = true; /* Next pass still needed */
+//                }
+//            }
+//            /* Array may be sorted and next pass not needed */
+//            if (needNextPass == false) {
+//                System.out.println("Array may be sorted and next pass not needed");
+//                break;
+//            }
+//            /* Show the list after sort */
+//            System.out.print("List after the  " + k + "' sort: ");
+//            for (int j = 0; j < list.length; j++) {
+//                System.out.print(list[j] + "\t");
+//            }
+//            System.out.println();
+//        }
+        boolean needNextPass = true;
+        for (int k = 0; k < list.length-1 && needNextPass; k++) {
+            needNextPass = false;
+            for (int i = list.length-1; i > k; i--) {
+                if (list[i-1] > list[i]) {
+                    int temp = list[i-1];
+                    list[i-1] = list[i];
+                    list[i] = temp;
+                    needNextPass = true;
+                }
+            }
+        }
+        for (int j = 0; j < list.length; j++) {
+            System.out.print(list[j] + "\t");
+        }
+        System.out.println();
+    }
+}
